99d574517313ec191e838d07a7eb42991a98db31
aliosmandurmaz <odurmaz.34@outlook.com>
2022-04-13 15:42:46 +0300

    first commit

---
+++ Kisiler Web Service/tum_kisiler.php
@@ -0,0 +1,48 @@
+<?php
+// arrray for JSON response
+$response = array();
+
+//DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE değişkenleri alınır.
+require_once __DIR__ .'/db_config.php';
+
+// Bağlantı oluşturuluyor.
+$baglanti = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
+
+//Bağlantı kontrolü yapılır.
+if (!$baglanti) {
+    die("Hatalı bağlantı:" .mysqli_connect_error());
+}
+
+$sqlsorgu = "SELECT * FROM kisiler";
+$result = mysqli_query($baglanti,$sqlsorgu);
+
+//Result kontrolü yap
+if (mysqli_num_rows($result) > 0) {
+    $response["kisiler"] = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+     
+        //temp user array
+        $kisiler["kisi_id"] = $row["kisi_id"];
+        $kisiler["kisi_ad"] = $row["kisi_ad"];
+        $kisiler["kisi_tel"] = $row["kisi_tel"];
+
+        //push single product into final response array
+        array_push($response["kisiler"],$kisiler);
+    }
+
+    //success
+    $response["success"] = 1;
+
+    //echoing JSON response
+    echo json_encode($response);
+}else {
+    //no products found
+    $response["success"] = 0;
+    $response["message"] = "No data found";
+    
+    echo json_encode($response);
+}
+
+//bağlantı koparılır.
+mysqli_close($baglanti);
+?>
---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define ('DB_USER', "u447336904_admin");
+define('DB_PASSWORD', "Ao387629");
+define('DB_DATABASE',"u447336904_aliosmandurmaz");
+define('DB_SERVER', "localhost");
+?>
